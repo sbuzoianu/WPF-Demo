40ff9df4ab0d6be3bf2e67db36d1cb6ea37d82f3
Stefan Buzoianu <stefan.buzoianu@hotmail.com>
2019-11-14 20:07:46 +0200

    New commit from Mac

--- WPF-Demo/MainWindow.xaml.cs
+++ WPF-Demo/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +24,7 @@ namespace WPF_Demo
         {
             Console.WriteLine("test");
             InitializeComponent();
+
         }
     }
 }
